1aeff08f1000436718967356b1142f4d48d34b85
SanketKumar75 <sanketdawar5875@gmail.com>
2021-05-29 16:47:34 +0530

    sg

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './App.css';
 import React from 'react';
 import {Route} from 'react-router-dom';
@@ -25,30 +25,5 @@ function App() {
         <FacultyHome />
         </Route>
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> front
-  );
-}
-
+  )}
 export default App;
